9ac9e54901809b5584a531921de51c90503a3169
Damir Arh <damir.arh@gmail.com>
2024-05-04 13:27:24 +0200

    Add DbContext to host builder

--- EfDesignTimeContext/Program.cs
+++ EfDesignTimeContext/Program.cs
@@ -1,8 +1,17 @@
+using EfDesignTimeContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<SampleDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Sample"));
+});
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
