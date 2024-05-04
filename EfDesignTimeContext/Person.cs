3599c26f11599dcc9ab373750b9f5bf195d9feee
Damir Arh <damir.arh@gmail.com>
2024-05-04 12:33:31 +0200

    Add project files.

---
+++ EfDesignTimeContext/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace EfDesignTimeContext;
+
+public class Person(string firstName, string lastName)
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+}
---
+++ EfDesignTimeContext/SampleDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfDesignTimeContext;
+
+public class SampleDbContext : DbContext
+{
+    public SampleDbContext(DbContextOptions<SampleDbContext> options)
+        : base(options) { }
+
+    public SampleDbContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=SampleFromOnConfiguring;Integrated Security=True"
+            );
+        }
+    }
+
+    public DbSet<Person> Persons { get; set; }
+}
