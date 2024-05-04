4ff9bce958324f6b3c735df055cee2ef8786f941
Damir Arh <damir.arh@gmail.com>
2024-05-04 13:41:02 +0200

    Add design time DbContext factory

---
+++ EfDesignTimeContext/DesignTimeSampleDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EfDesignTimeContext;
+
+public class DesignTimeSampleDbContextFactory : IDesignTimeDbContextFactory<SampleDbContext>
+{
+    public SampleDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<SampleDbContext>();
+        optionsBuilder.UseSqlServer(
+            @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=SampleFromFactory;Integrated Security=True"
+        );
+
+        return new SampleDbContext(optionsBuilder.Options);
+    }
+}
